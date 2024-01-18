fd12adbdc095319d513ed15760aaf7eabac9b482
segunojo1 <olusegunkd2017@gmail.com>
2024-01-18 08:07:50 -0800

    names

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    module_names = dir(hidden_4)
+    names = [n for n in module_names if not n.startswith('__')]
+    names.sort()
+    for i in names:
+        print("{}".format(names[i]))
