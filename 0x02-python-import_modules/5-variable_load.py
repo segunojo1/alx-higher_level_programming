f7c46f544b13267bf5631f2f86858246263a684d
segunojo1 <olusegunkd2017@gmail.com>
2024-01-18 08:23:55 -0800

    test

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
-    val = variable_load_5.__dict__['a']
-    print("{}".format(val))
+    from variable_load_5 import a
+    #val = variable_load_5.__dict__['a']
+    print("{}".format(a))
