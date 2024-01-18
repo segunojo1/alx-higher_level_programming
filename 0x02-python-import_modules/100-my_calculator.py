cf60905428bd97508122c88c9d2a54ca28ab8a1e
segunojo1 <olusegunkd2017@gmail.com>
2024-01-18 08:46:21 -0800

    calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    operators = ['+', '-', '*', '/']
+    if not len(sys.argv) == 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif len(sys.argv == 4):
+        if not sys.argv[3] in operators:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    funcs = {"+": add, "-": sub, "*": mul, "/": div}
+    a = sys.argv[1]
+    b = sys.argv[3]
+    opp = sys.argv[2]
+    res = funcs[opp](a, b)
