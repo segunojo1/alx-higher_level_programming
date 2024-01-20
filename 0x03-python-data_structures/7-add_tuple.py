b667ac77c9fcaaae46f22d2c4cb1839ffceb36e1
segunojo1 <olusegunkd2017@gmail.com>
2024-01-20 06:12:09 -0800

    add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a0, a1, b0, b1 = 0, 0, 0, 0
+    if len(tuple_a) > 1:
+        a0 = tuple_a[0]
+        a1 = tuple_a[1]
+    elif len(tuple_a) == 1:
+        a0 = tuple_a[0]
+        a1 = 0
+    if len(tuple_b) > 1:
+        b0 = tuple_b[0]
+        b1 = tuple_b[1]
+    elif len(tuple_b) == 1:
+        b0 = tuple_b[0]
+        b1 = 0
+    return (a0 + b0, a1 + b1)
