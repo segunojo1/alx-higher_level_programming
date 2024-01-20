ca377962e1ef20f528e828c8df06ee34fe711741
segunojo1 <olusegunkd2017@gmail.com>
2024-01-20 04:12:06 -0800

    new in lst

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+def new_in_list(my_list, idx, element):
+    if my_list:
+        new_list = my_list.copy()
+        if idx < 0:
+            return new_list
+        elif idx > len(my_list) - 1:
+            return new_list
+        else:
+            new_list[idx] = element
+            return new_list
