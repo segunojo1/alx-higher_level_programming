a7a27fa4e97d20c08920e056e0e425912b60c75f
segunojo1 <olusegunkd2017@gmail.com>
2024-01-20 05:33:59 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for lis in matrix:
-            for lis2 in lis:
-                print("{:d}".format(lis2), end=" ")
-            print('')
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            print("{:s}".format(""))
+        else:
+            for lis in matrix:
+                for lis2 in lis:
+                    print("{:d}".format(lis2), end=" ")
+                print('')
