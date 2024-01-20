aee9e83c20e8e3dbaf3ab07317912f3ea87b718d
segunojo1 <olusegunkd2017@gmail.com>
2024-01-20 06:53:02 -0800

    fix max int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,7 +8,3 @@ def max_integer(my_list=[]):
             if i > j and i > max_num:
                 max_num = i
     return max_num
-
-my_list = [-23, -104, -5, -13, -93]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
