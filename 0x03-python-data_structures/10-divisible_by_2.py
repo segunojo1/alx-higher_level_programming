af37c492bac36a540dabc4d131763c2550f11f3f
segunojo1 <olusegunkd2017@gmail.com>
2024-01-20 07:08:46 -0800

    div by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for itm in range(0, len(my_list)):
+        if my_list[itm] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
