97e579768cb50db1f49cfe05dcc3a39c008a0749
segunojo1 <olusegunkd2017@gmail.com>
2024-02-22 14:01:32 +0100

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,10 @@
+"""Base class"""
+class Base:
+    """class instance"""
+    __nb_objects = 0
+        def __init__(self, id=None):
+            if id is not None:
+                self.id = id
+            else:
+                __nb_objects = __nb_objects + 1
+                self.id = __nb_objects
