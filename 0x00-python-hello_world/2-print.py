9cd662453f42eb166878b280580e8a286c61ae73
segunojo1 <olusegunkd2017@gmail.com>
2024-03-16 20:50:38 +0100

    commit

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 print("\"Programming is like building a multilingual puzzle")
 
-def greet(name):
-    print("Hello {name}")
-
-
-greet("Peculiar")
-greet("jhdhd")
-
-number = 5
-
-print("{number}")
