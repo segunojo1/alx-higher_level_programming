3481c2c69174da3e673f3add385bb1904b808acb
segunojo1 <olusegunkd2017@gmail.com>
2024-03-22 04:31:12 +0100

    read file

--- 0x0A-python-inheritance/prac.py
+++ 0x0A-python-inheritance/prac.py
@@ -21,3 +21,30 @@ class BaseGeometry:
         else:
             if value <= 0:
                 raise ValueError("<name> must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area():
+        return self.__width * self.__height
+
+    def __str__(self):
+        str = ""
+        return str
+
+    def __repr__(self):
+        print(f"[Rectangle] {self.__width}/{self.__height}")
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__()
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area():
+        return self.__size ** 2
