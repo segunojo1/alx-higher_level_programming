a3f0c411c7d6b2b4f425c1dd003980e3350f7973
segunojo1 <olusegunkd2017@gmail.com>
2024-03-22 21:38:16 +0100

    writee

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,5 @@
 def write_file(filename="", text=""):
     """write file"""
     with open(filename, encoding="utf-8") as f:
-        fwrite = f.write(filename, text)
-        return len(fwrite)
+        f.write(text)
+        return len(text)
