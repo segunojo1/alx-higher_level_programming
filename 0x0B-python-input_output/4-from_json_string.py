a070c0e03a3dc6ef15e95467b9a9a97578d4bac9
segunojo1 <olusegunkd2017@gmail.com>
2024-03-22 23:17:29 +0100

    fix json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,5 +7,7 @@ def from_json_string(my_str):
     """function for jsoon string
     Args:
     my_str: str to convert
+    Returns:
+    any: the json to object
     """
-    return JSONDecoder.decode(my_str)
+    return JSONDecoder().decode(my_str)
