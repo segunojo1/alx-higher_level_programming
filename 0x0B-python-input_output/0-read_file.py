11c6ef5ad67d5a2cb8f91877fabb0aff348aeca3
segunojo1 <olusegunkd2017@gmail.com>
2024-03-22 22:07:13 +0100

    FIXX

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines the read file function"""
 
+
 def read_file(filename=""):
     """reads a file"""
     with open(filename, encoding="utf-8") as f:
         filedata = f.read()
         print(filedata, end="")
-        
