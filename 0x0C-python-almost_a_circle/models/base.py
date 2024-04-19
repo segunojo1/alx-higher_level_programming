5fef83f75b59038c4f76091a578e5ec9d8b0b25e
segunojo1 <olusegunkd2017@gmail.com>
2024-04-19 18:42:49 +0100

    pycode

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 # base.py
 """Base class model for the rest of the project"""
+
+
 class Base:
-
-
     """Represent the base model.
     Represents the "base" for all other classes in project 0x0C*.
     Attributes:
         __nb_objects (int): The number of instantiated Bases.
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
-        
         """Initialize a new Base.
         Args:
             id (int): The identity of the new Base.
