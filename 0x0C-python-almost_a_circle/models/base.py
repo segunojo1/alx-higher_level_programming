04281fca9a4ad21bd61133e6ca3ad3fece1f7e42
segunojo1 <olusegunkd2017@gmail.com>
2024-04-19 18:01:46 +0100

    indent fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,12 +2,12 @@
 # base.py
 """Base class model for the rest of the project"""
 class Base:
-    """class instanc of the basee"""
+    """class instance of the basee"""
     __nb_objects = 0
-        def __init__(self, id=None):
-            """initialization of the base class"""
-            if id is not None:
-                self.id = id
-            else:
-                __nb_objects = __nb_objects + 1
-                self.id = __nb_objects
+    def __init__(self, id=None):
+        """initialization of the base class"""
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects = __nb_objects + 1
+            self.id = __nb_objects
