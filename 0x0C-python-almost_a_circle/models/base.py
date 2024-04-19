52976d7814194daf66deb8176be1de0762bcd25d
segunojo1 <olusegunkd2017@gmail.com>
2024-04-19 18:06:39 +0100

    indent

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
         __nb_objects (int): The number of instantiated Bases.
     """
     __nb_objects = 0
-   def __init__(self, id=None):
+    def __init__(self, id=None):
         """Initialize a new Base.
         Args:
             id (int): The identity of the new Base.
