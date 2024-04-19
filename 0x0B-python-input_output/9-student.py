fdd99e47d7ae5b4e2fd297cd7804865c8551e801
segunojo1 <olusegunkd2017@gmail.com>
2024-04-19 16:11:02 +0100

    9-student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module for student"""
+
+class Student:
+    """defines a student class"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation with some values"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returs dicc rep of self"""
+        json.dumps(self)
