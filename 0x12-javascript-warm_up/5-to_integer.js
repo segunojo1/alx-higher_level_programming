f051b4d32b64762203aea2f65f1be1c1a884c217
segunojo1 <olusegunkd2017@gmail.com>
2024-04-22 22:58:15 +0100

    int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const {argv} = require('node:process');
+const argv0 = argv[2]
+if (Number.isInteger(Number(argv[2]))) {
+  console.log('My number: ', argv0);
+} else {
+  console.log('Not a number');
+}
