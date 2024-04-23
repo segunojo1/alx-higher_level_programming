7789d17b5be0e13fa21a8a7299605b856ac232dd
segunojo1 <olusegunkd2017@gmail.com>
2024-04-23 02:16:42 +0100

    semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ function factorial (num) {
   if (num === 0 || num === 1 || isNaN(num)) {
     return (1);
   } else {
-    return num * factorial (num -1);
+    return num * factorial(num - 1);
   }
 }
 console.log(factorial(parseInt(argv[2])));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (len <= 3) {
   console.log(0);
 } else {
   const args = process.argv.slice(2, len);
-  const sorted_args = args.sort((a, b) => a - b);
-  console.log(sorted_args[len - 2]);
+  const sortedArgs = args.sort((a, b) => a - b);
+  console.log(sortedArgs[len - 2]);
 }
