b17273903aead8ac7372c2ebd93db31c07817d99
segunojo1 <olusegunkd2017@gmail.com>
2024-04-23 08:31:28 -0700

    rec

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
       return;
-    };
+    }
     this.width = w;
     this.height = h;
   }
