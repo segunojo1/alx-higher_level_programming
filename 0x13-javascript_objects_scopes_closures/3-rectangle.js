a1f2edc9002eff87b90642ee1a95848ab6f92fc1
segunojo1 <olusegunkd2017@gmail.com>
2024-04-23 08:48:41 -0700

    semi

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
       return;
-    } else {
-      this.width = w;
-      this.height = h;
     }
+    this.width = w;
+    this.height = h;
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let row = '';
