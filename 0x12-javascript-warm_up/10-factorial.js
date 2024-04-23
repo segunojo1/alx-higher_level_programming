d4ea113d2c199b914bec801e9efa8aeced0c57a7
segunojo1 <olusegunkd2017@gmail.com>
2024-04-23 02:04:05 +0100

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
-!#/usr/bin/node
-const { argv } = require(node:process);
+#!/usr/bin/node
+const { argv } = require('node:process');
 function factorial (num) {
-  if (num === 0 || num === 1 || isNan(num)) {
+  if (num === 0 || num === 1 || isNaN(num)) {
     return (1);
   } else {
     return num * factorial (num -1);
   }
 }
-factorial(parseInt(argv[2]);
+console.log(factorial(parseInt(argv[2])));
