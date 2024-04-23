a707227784820b37e14497802f6074e23bc7eb5a
segunojo1 <olusegunkd2017@gmail.com>
2024-04-23 10:55:26 -0700

    done

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function convertToBase(number) {
-    return number < base ? String(number) : convertToBase(number / base | 0) + String(number % base);
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
