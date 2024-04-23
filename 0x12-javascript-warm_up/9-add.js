d9e01ac5f9fcc24c1725286b8c5fa97ebb96e07e
segunojo1 <olusegunkd2017@gmail.com>
2024-04-22 17:36:04 -0700

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process')
 function add(a, b) {
-  let sum = a + b;
+  let intA = parseInt(a);
+  let intB = parseInt(b);
+  let sum = intA + intB;
   return (sum);
 }
 if (!Number.isInteger(Number(argv[2])) || !Number.isInteger(Number(argv[3]))) {
