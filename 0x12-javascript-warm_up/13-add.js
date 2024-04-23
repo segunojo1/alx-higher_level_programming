bf327dfdfc75c72ddf10053a4f2458695190e011
segunojo1 <olusegunkd2017@gmail.com>
2024-04-22 18:28:29 -0700

    13-add

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+add = function (a, b) {
+  const intA = parseInt(a);
+  const intB = parseInt(b);
+  const sum = intA + intB;
+  return (sum);
+}
+if (!Number.isInteger(Number(argv[2])) || !Number.isInteger(Number(argv[3]))) {
+  console.log('NaN');
+} else {
+  const sum = add(argv[2], argv[3]);
+  console.log(sum);
+}
+
