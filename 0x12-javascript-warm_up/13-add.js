d65a806f4670b9d8f89f2fc8f09b9a55fc079d61
segunojo1 <olusegunkd2017@gmail.com>
2024-04-22 18:33:05 -0700

    export

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-add = function (a, b) {
+module.exports = function (a, b) {
   const intA = parseInt(a);
   const intB = parseInt(b);
   const sum = intA + intB;
